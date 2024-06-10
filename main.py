497c3b482fce8be87fb385cbea9036bae0ae256d
knightmoon <ankitakshay78@gmail.com>
2024-06-10 09:34:36 +0530

    get longitude and latitude

---
+++ main.py
@@ -0,0 +1,18 @@
+from geopy.geocoders import Nominatim
+
+# Initialize the Nominatim geocoder
+geolocator = Nominatim(user_agent="geoapiExercises")
+
+# Function to get latitude and longitude
+def get_lat_lon(place_name):
+    location = geolocator.geocode(place_name)
+    if location:
+        return location.latitude, location.longitude
+    else:
+        return None, None
+
+# Example usage
+place_name = input("Enter place name: ")
+latitude, longitude = get_lat_lon(place_name)
+
+print(f"The latitude and longitude of {place_name} are {latitude}, {longitude}")
